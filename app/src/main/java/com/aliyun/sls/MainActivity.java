cecb9d4290e5b5433aa3a3e6768df1ead877b1ec
zuoqin <zuoqin.wz@alibaba-inc.com>
2017-10-30 17:15:59 +0800

    Log : 添加STS 使用例子

--- app/src/main/java/com/aliyun/sls/MainActivity.java
+++ app/src/main/java/com/aliyun/sls/MainActivity.java
@@ -11,12 +11,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import com.aliyun.sls.android.sdk.ClientConfiguration;
 import com.aliyun.sls.android.sdk.LOGClient;
 import com.aliyun.sls.android.sdk.LogException;
+import com.aliyun.sls.android.sdk.SLSLog;
+import com.aliyun.sls.android.sdk.core.auth.StsTokenCredentialProvider;
 import com.aliyun.sls.android.sdk.model.Log;
 import com.aliyun.sls.android.sdk.model.LogGroup;
 import com.aliyun.sls.android.sdk.core.callback.CompletedCallback;
-import com.aliyun.sls.android.sdk.core.auth.PlainTextAKSKCredentialProvider;
 import com.aliyun.sls.android.sdk.utils.IPService;
 import com.aliyun.sls.android.sdk.request.PostLogRequest;
 import com.aliyun.sls.android.sdk.result.PostLogResult;
@@ -35,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
     public String accessKeySecret = "******";
     public String project = "******";
     public String logStore = "******";
-
     public String source_ip = "";
 
 
@@ -78,13 +80,73 @@ public class MainActivity extends AppCompatActivity {
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 asyncUploadLog(source_ip);
             }
         });
 
     }
 
+    /*
+     *  推荐使用的方式，直接调用异步接口，通过callback 获取回调信息
+     */
+    private void asyncUploadLog(@Nullable String ip) {
+        if (TextUtils.isEmpty(ip)){
+            Toast.makeText(MainActivity.this,"请先获取ip地址",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //移动端是不安全环境，不建议把ak，sk保存在本地。建议使用STS方式。具体参见
+        //https://help.aliyun.com/document_detail/60899.html?spm=5176.doc29090.6.661.3S92j7
+        String STS_AK = "******";
+        String STS_SK = "******";
+        String STS_TOKEN = "******";
+        StsTokenCredentialProvider credentialProvider =
+                new StsTokenCredentialProvider(STS_AK,STS_SK,STS_TOKEN);
+
+        ClientConfiguration conf = new ClientConfiguration();
+        conf.setConnectionTimeout(15 * 1000); // 连接超时，默认15秒
+        conf.setSocketTimeout(15 * 1000); // socket超时，默认15秒
+        conf.setMaxConcurrentRequest(5); // 最大并发请求书，默认5个
+        conf.setMaxErrorRetry(2); // 失败后最大重试次数，默认2次
+        SLSLog.enableLog(); // log打印在控制台
+        LOGClient logClient = new LOGClient(endpoint, credentialProvider, conf);
+        /* 创建logGroup */
+        LogGroup logGroup = new LogGroup("sls test", ip);
+
+        /* 存入一条log */
+        Log log = new Log();
+        log.PutContent("current time ", "" + System.currentTimeMillis() / 1000);
+        log.PutContent("content", "this is a log");
+
+        logGroup.PutLog(log);
+
+        try{
+            PostLogRequest request = new PostLogRequest(project,logStore,logGroup);
+            logClient.asyncPostLog(request, new CompletedCallback<PostLogRequest, PostLogResult>() {
+                @Override
+                public void onSuccess(PostLogRequest request, PostLogResult result) {
+                    Message message = Message.obtain(handler);
+                    message.what = HANDLER_MESSAGE_UPLOAD_SUCCESS;
+                    message.sendToTarget();
+                }
+
+                @Override
+                public void onFailure(PostLogRequest request, LogException exception) {
+                    Message message = Message.obtain(handler);
+                    message.what = HANDLER_MESSAGE_UPLOAD_FAILED;
+                    message.obj = exception.getMessage();
+                    message.sendToTarget();
+                }
+            });
+        }catch (LogException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+     *  0.3.1 以前的使用方式(不推荐，但兼容)
+     */
     private void sampleUploadLog(@Nullable String ip) {
         if (TextUtils.isEmpty(ip)){
             Toast.makeText(MainActivity.this,"请先获取ip地址",Toast.LENGTH_SHORT).show();
@@ -123,45 +185,5 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    private void asyncUploadLog(@Nullable String ip) {
-        if (TextUtils.isEmpty(ip)){
-            Toast.makeText(MainActivity.this,"请先获取ip地址",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        PlainTextAKSKCredentialProvider plainTextAKSKCredentialProvider =
-                new PlainTextAKSKCredentialProvider(accesskeyID,accessKeySecret);
 
-        LOGClient logClient = new LOGClient(endpoint, plainTextAKSKCredentialProvider, null);
-        /* 创建logGroup */
-        LogGroup logGroup = new LogGroup("sls test", ip);
-
-        /* 存入一条log */
-        Log log = new Log();
-        log.PutContent("current time ", "" + System.currentTimeMillis() / 1000);
-        log.PutContent("content", "this is a log");
-
-        logGroup.PutLog(log);
-
-        try{
-            PostLogRequest request = new PostLogRequest(project,logStore,logGroup);
-            logClient.asyncPostLog(request, new CompletedCallback<PostLogRequest, PostLogResult>() {
-                @Override
-                public void onSuccess(PostLogRequest request, PostLogResult result) {
-                    Message message = Message.obtain(handler);
-                    message.what = HANDLER_MESSAGE_UPLOAD_SUCCESS;
-                    message.sendToTarget();
-                }
-
-                @Override
-                public void onFailure(PostLogRequest request, LogException exception) {
-                    Message message = Message.obtain(handler);
-                    message.what = HANDLER_MESSAGE_UPLOAD_FAILED;
-                    message.obj = exception.getMessage();
-                    message.sendToTarget();
-                }
-            });
-        }catch (LogException e){
-            e.printStackTrace();
-        }
-    }
 }
