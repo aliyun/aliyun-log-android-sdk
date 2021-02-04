2893399a482b65441031b7ae154a97ea8a7631d2
chaolee50 <chaolee50@outlook.com>
2021-02-04 12:49:01 +0800

    [add] auto update server time

--- aliyun_sls_android_producer/src/main/java/com/aliyun/sls/android/producer/LogProducerHttpTool.java
+++ aliyun_sls_android_producer/src/main/java/com/aliyun/sls/android/producer/LogProducerHttpTool.java
@@ -7,10 +7,13 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class LogProducerHttpTool {
 
-    private static final String VERSION = "sls-android-sdk_v2.5.10";
+    private static final String VERSION = "sls-android-sdk_v2.5.11";
+
+    protected static AtomicLong localServerDeltaTime = new AtomicLong(0);
 
     static public int android_http_post(String urlString, String[] header, byte[] body) {
         try {
@@ -33,11 +36,22 @@ public class LogProducerHttpTool {
             out.write(body);
             out.flush();
             out.close();
+            String timeVal = httpConn.getHeaderField("x-log-time");
+            if (timeVal != null && !"".equals(timeVal)) {
+                long serverTime = Long.parseLong(timeVal);
+                if (serverTime > 1500000000 && serverTime < 4294967294L) {
+                    long sysTime = System.currentTimeMillis() / 1000;
+                    long deltaTime = serverTime - sysTime;
+                    if (deltaTime > 600 || deltaTime < -600) {
+                        localServerDeltaTime.set(deltaTime);
+                    }
+                }
+            }
             int responseCode = httpConn.getResponseCode();
             if (responseCode / 100 == 2) return responseCode;
             BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getErrorStream()));
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
