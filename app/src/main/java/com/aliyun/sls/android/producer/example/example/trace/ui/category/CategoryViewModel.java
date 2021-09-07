63db7fe0b3bf6c27c3cd26432286bff070d1bb10
gzbleu <gzbleu@gmail.com>
2021-09-07 14:57:45 +0800

    feat: category

--- app/src/main/java/com/aliyun/sls/android/producer/example/example/trace/ui/category/CategoryViewModel.java
+++ app/src/main/java/com/aliyun/sls/android/producer/example/example/trace/ui/category/CategoryViewModel.java
@@ -4,20 +4,38 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.aliyun.sls.android.producer.example.example.trace.http.ApiClient;
+import com.aliyun.sls.android.producer.example.example.trace.model.ItemModel;
+
+import java.util.List;
+
 /**
  * @author gordon
  * @date 2021/09/01
  */
 public class CategoryViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<List<ItemModel>> itemMutableLiveData;
 
     public CategoryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        itemMutableLiveData = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<ItemModel>> getItemModelList() {
+        return itemMutableLiveData;
+    }
+
+    public void update() {
+        ApiClient.getCategory(new ApiClient.ApiCallback<List<ItemModel>>() {
+            @Override
+            public void onSuccess(List<ItemModel> response) {
+                itemMutableLiveData.setValue(response);
+            }
+
+            @Override
+            public void onError(int code, String error) {
+
+            }
+        });
     }
 }
