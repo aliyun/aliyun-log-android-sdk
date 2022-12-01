332d36419aa929d79984d105136191c01b66ca32
gzbleu <gzbleu@gmail.com>
2022-12-01 21:30:35 +0800

    feat: refine maven publish scripts

--- publish-gradleplugin.sh
+++ publish-gradleplugin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./gradlew :aliyun_sls_android_gradle_plugin:clean
 ./gradlew :aliyun_sls_android_gradle_plugin:assemble
-./gradlew :aliyun_sls_android_gradle_plugin:publish
+./gradlew :aliyun_sls_android_gradle_plugin:publishToSonatype closeAndReleaseStagingRepository
