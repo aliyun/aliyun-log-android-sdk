c0369408ba51dc6ab0b2b4925313291ce5c01b19
gzbleu <gzbleu@gmail.com>
2023-05-09 11:28:27 +0800

    feat: remove gradle plugin

--- publish-gradleplugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# 定义版本号
-version=0.0.3
-moduleName=sls-android-gradle-plugin
-
-./gradlew :aliyun_sls_android_gradle_plugin:clean -Pversion=${version} -PmoduleName=${moduleName}
-./gradlew :aliyun_sls_android_gradle_plugin:assemble -Pversion=${version} -PmoduleName=${moduleName}
-./gradlew :aliyun_sls_android_gradle_plugin:publishToSonatype closeAndReleaseStagingRepository -Pversion=${version} -PmoduleName=${moduleName}
-#./gradlew :aliyun_sls_android_gradle_plugin:publishToMavenLocal -Pversion=${version} -PmoduleName=${moduleName}
