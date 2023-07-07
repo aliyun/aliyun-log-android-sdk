d0fd26ec18d664a089994125044d0919c16e8a53
gzbleu <gzbleu@gmail.com>
2023-07-07 15:50:11 +0800

    feat: refine publish scripts & support webview-instrumentation

--- publish.sh
+++ publish.sh
@@ -14,19 +14,42 @@ function current_tag () {
     git -C "$folder" rev-parse --short HEAD
 }
 
+#function git_version() {
+#    folder="$(pwd)"
+#
+#    # get tag name that points to the current HEAD commit
+#    git_tag=$(git -C "$folder" tag --points-at HEAD 2>/dev/null)
+#
+#    # if no tag found, get the branch name
+#    if [ -z "$git_tag" ]; then
+#        git_branch=$(git -C "$folder" symbolic-ref -q --short HEAD 2>/dev/null)
+#
+#        # if not on a branch, get the short commit hash
+#        if [ -z "$git_branch" ]; then
+#            git_commit=$(git -C "$folder" rev-parse --short HEAD 2>/dev/null)
+#            echo "$git_commit"
+#        else
+#            echo "$git_branch"
+#        fi
+#    else
+#        echo "$git_tag"
+#    fi
+#}
+
 # map tag to module name
 function module_name() {
   case $1 in
-    *blockdetection*)     echo "aliyun_sls_android_blockdetection";;
-    *core*)               echo "aliyun_sls_android_core";;
-    *crashreporter*)      echo "aliyun_sls_android_crashreporter";;
-    *network_diagnosis*)  echo "aliyun_sls_android_network_diagnosis";;
-    *okhttp*)             echo "aliyun_sls_android_okhttp";;
-    *ot-ktx*)             echo "aliyun_sls_android_ot_ktx";;
-    *ot*)                 echo "aliyun_sls_android_ot";;
-    *producer*)           echo "aliyun_sls_android_producer";;
-    *trace*)              echo "aliyun_sls_android_trace";;
-    *)                    echo "not_support";;
+    *webview-instrumentation*)    echo "webview_instrumentation";;
+    *blockdetection*)             echo "aliyun_sls_android_blockdetection";;
+    *core*)                       echo "aliyun_sls_android_core";;
+    *crashreporter*)              echo "aliyun_sls_android_crashreporter";;
+    *network_diagnosis*)          echo "aliyun_sls_android_network_diagnosis";;
+    *okhttp*)                     echo "aliyun_sls_android_okhttp";;
+    *ot-ktx*)                     echo "aliyun_sls_android_ot_ktx";;
+    *ot*)                         echo "aliyun_sls_android_ot";;
+    *producer*)                   echo "aliyun_sls_android_producer";;
+    *trace*)                      echo "aliyun_sls_android_trace";;
+    *)                            echo "not_support";;
   esac
 }
 
