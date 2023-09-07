33528f500783011b70b92556562cce49d9ee9ca4
gzbleu <gzbleu@gmail.com>
2023-09-07 12:28:47 +0800

    feat: add new crashreporter

---
+++ aliyun_sls_android_crashreporter2/src/main/java/com/aliyun/sls/android/crashreporter/CrashReporter.java
@@ -0,0 +1,117 @@
+package com.aliyun.sls.android.crashreporter;
+
+import java.io.File;
+
+import android.app.Application;
+import android.os.Bundle;
+import com.aliyun.sls.android.crashreporter.parser.CrashFileHelper;
+import com.uc.crashsdk.export.CrashApi;
+import com.uc.crashsdk.export.ICrashClient;
+
+/**
+ * @author yulong.gyl
+ * @date 2023/9/7
+ */
+public final class CrashReporter implements ICrashClient {
+    private static final String TAG = "CrashReporter";
+    private static final String PATH_ROOT = "sls_rum" + File.separator + "crashreporter";
+    public static final String PATH_ITRACE_LOGS = PATH_ROOT + File.separator + "itrace_logs";
+    public static final String PATH_ITRACE_TAGS = PATH_ROOT + File.separator + "itrace_tags";
+
+    private final Application application;
+
+    private CrashApi crashApi;
+
+    private CrashReporter(Application application) {
+        this.application = application;
+    }
+
+    public void init(boolean debuggable) {
+        String fileDirName = application.getFilesDir().getName();
+        final Bundle args = new Bundle();
+        // 路径配置
+        args.putBoolean("mBackupLogs", false);
+        args.putBoolean("mBackupLogs", debuggable);
+        //args.putString("mLogsBackupPathName", new File(rootPath, "backup").getAbsolutePath());
+        args.putString("mTagFilesFolderName", fileDirName + File.separator + PATH_ITRACE_TAGS);
+        args.putString("mCrashLogsFolderName", fileDirName + File.separator + PATH_ITRACE_LOGS);
+
+        // crash 类型配置
+        // 开启 Java 监控
+        args.putBoolean("enableJavaLog", true);
+        // 开启 native 监控
+        args.putBoolean("enableNativeLog", true);
+        // 关闭 unexp 监控, itrace 还不稳定, 用户理解成本会有点高
+        args.putBoolean("enableUnexpLog", false);
+        // anr监控策略，所有 rom 生效
+        args.putInt("mAnrTraceStrategy", 2);
+
+        // crash 不再冒泡
+        args.putBoolean("mCallJavaDefaultHandler", false);
+        args.putBoolean("mCallNativeDefaultHandler", true);
+
+        // 关闭加密
+        args.putBoolean("mEncryptLog", false);
+        // 关闭压缩
+        args.putBoolean("mZipLog", false);
+        //args.putString("mJavaCrashLogFileName", "java_" + System.currentTimeMillis() + "_java.log");
+        //args.putString("mNativeCrashLogFileName", "native_" + System.currentTimeMillis() + "_jni.log");
+        //args.putString("mUnexpCrashLogFileName", "unexp_" + System.currentTimeMillis() + "_unexp.log");
+
+        args.putBoolean("useApplicationContext", true);
+
+        // 不上报, 通过 sls 上报
+        args.putBoolean("mEnableStatReport", true);
+        args.putBoolean("mSyncUploadSetupCrashLogs", false);
+        args.putBoolean("mSyncUploadLogs", false);
+        //args.putLong("mDisableSignals", (1 << 14) | (1 << 1));
+        //args.putLong("mDisableBackgroundSignals", 1 << 14);
+        args.putInt("uploadLogDelaySeconds", -1);
+        args.putInt("mInfoSaveFrequency", 3);
+
+        // 防止 uc 计算 crc
+        //args.putString("mBuildId", AppUtils.getAppVersion(application));
+
+        //final String appId = options.instanceId;
+        crashApi = CrashApi.createInstanceEx(application, "sls-internal", false, args, this);
+        crashApi.setCrashStatReporter((uuid, stat) -> {
+            //if (options.debuggable) {
+            //    SLSLog.v(TAG, "report dau stat, stat: " + stat);
+            //}
+
+            CrashFileHelper.scanAndReport(application);
+            return true;
+        });
+        crashApi.setForeground(true);
+    }
+
+    @Override
+    public void onLogGenerated(File file, String s) {
+        CrashFileHelper.parseCrashFile(application, file, s);
+    }
+
+    @Override
+    public void onClientProcessLogGenerated(String s, File file, String s1) {
+
+    }
+
+    @Override
+    public File onBeforeUploadLog(File file) {
+        return file;
+    }
+
+    @Override
+    public void onCrashRestarting(boolean b) {
+
+    }
+
+    @Override
+    public void onAddCrashStats(String s, int i, int i1) {
+
+    }
+
+    @Override
+    public String onGetCallbackInfo(String s, boolean b) {
+        return null;
+    }
+}
---
+++ aliyun_sls_android_crashreporter2/src/main/java/com/aliyun/sls/android/crashreporter/parser/CrashFileHelper.java
@@ -0,0 +1,170 @@
+package com.aliyun.sls.android.crashreporter.parser;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import android.content.Context;
+import android.text.TextUtils;
+import com.aliyun.sls.android.producer.utils.TimeUtils;
+
+import static android.util.Log.w;
+import static java.lang.String.format;
+
+/**
+ * @author gordon
+ * @date 2022/5/8
+ */
+public class CrashFileHelper {
+    private static final String TAG = "CrashFileHelper";
+
+    private static final String PATH_ROOT = "sls_rum" + File.separator + "crashreporter";
+    public static final String PATH_ITRACE_LOGS = PATH_ROOT + File.separator + "itrace_logs";
+    public static final String PATH_ITRACE_TAGS = PATH_ROOT + File.separator + "itrace_tags";
+
+    public static void scanAndReport(Context context) {
+        CrashFileHelper helper = new CrashFileHelper();
+        final File crashLogFile = new File(context.getFilesDir(), PATH_ITRACE_LOGS);
+        helper.scanAndReport(context, crashLogFile);
+    }
+
+    public static void parseCrashFile(Context context, File file, String type) {
+        CrashFileHelper helper = new CrashFileHelper();
+        helper.parseTraceFile(context, type, file);
+    }
+
+    public void parseTraceFile(Context context, String type, File file) {
+        if (TextUtils.isEmpty(type)) {
+            w(TAG, format("parseTraceFile, type is empty. type: %s", type));
+            return;
+        }
+
+        if (null == file || !file.exists()) {
+            w(TAG, format("parseTraceFile, file is not exists or null. type: %s, file: %s"
+                , type
+                , (null != file ? file.getAbsolutePath() : "null"), type));
+            return;
+        }
+
+        if ("jni".equals(type)) {
+            type = "native";
+        }
+        LogParserResult result = LogParser.getInstance().parser(context, file, type);
+        onLogParsedEnd(type, file, result);
+    }
+
+    private void onLogParsedEnd(final String type, final File file, final LogParserResult result) {
+        //if (options.debuggable) {
+        //    v(TAG, SLSLog.format("onLogParsedEnd. type: %s, content length: %d", type, content.length()));
+        //}
+
+        String time = result.getString("time");
+        String id = result.getString("id");
+        String catId = result.getString("catId");
+        result.remove("time");
+        result.remove("id");
+        long start = TimeUtils.getInstance().getTimeInMillis();
+        if (!TextUtils.isEmpty(time)) {
+            start = TimeUnit.MILLISECONDS.toNanos(parseTime(time));
+        }
+
+        //List<Attribute> attributes = new ArrayList<>();
+        //Iterator<String> it = result.keys();
+        //while (it.hasNext()) {
+        //    String key = it.next();
+        //    attributes.add(
+        //        Attribute.of("ex." + key, result.getString(key))
+        //    );
+        //}
+        //
+        //final String topActivity = AppUtils.getTopActivity();
+        //final String eventName = "Application crashed";
+        //SpanBuilder builder = SLSAndroidRum.spanBuilder(eventName)
+        //    .setStart(start)
+        //    .addAttribute(
+        //        Attribute.of(
+        //            Pair.create("title", eventName),
+        //            Pair.create("state", type),
+        //            Pair.create("page.name", TextUtils.isEmpty(topActivity) ? "" : topActivity),
+        //            Pair.create("t", "error"),
+        //            Pair.create("ex.type", "crash"),
+        //            Pair.create("ex.sub_type", type),
+        //            Pair.create("ex.id", id),
+        //            Pair.create("ex.catId", catId),
+        //            Pair.create("ex.file", file.getName())
+        //        )
+        //    )
+        //    .addAttribute(attributes);
+        //Span span = builder.build();
+        //final boolean ret = span.end();
+        //if (ret) {
+        //    //noinspection ResultOfMethodCallIgnored
+        //    file.delete();
+        //} else {
+        //    w(TAG, "send crash span to log producer error.");
+        //}
+    }
+
+    private String obtainTime(String info) {
+        try {
+            String time = info.split("time:")[1].trim();
+            return time.substring(0, time.length() - 1);
+        } catch (Throwable t) {
+            return null;
+        }
+    }
+
+    private long toLongTime(String time) {
+        try {
+            return Long.parseLong(time);
+        } catch (Throwable t) {
+            return System.currentTimeMillis();
+        }
+    }
+
+    private long parseTime(String time) {
+        try {
+            return new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).parse(time).getTime();
+        } catch (Throwable e) {
+            return System.currentTimeMillis();
+        }
+    }
+
+    private void scanAndReport(Context context, File logFolder) {
+        //if (options.debuggable) {
+        //    SLSLog.v(TAG, "scanAndReport. logFolder: " + logFolder.getAbsolutePath());
+        //}
+
+        final File[] files = logFolder.listFiles();
+        if (null == files) {
+            //if (options.debuggable) {
+            //    SLSLog.v(TAG, "scanAndReport. folder is empty.");
+            //}
+            return;
+        }
+        //else if (options.debuggable) {
+        //    SLSLog.v(TAG, "scanAndReport. file count: " + files.length);
+        //}
+
+        for (File file : files) {
+            //if (options.debuggable) {
+            //    SLSLog.v(TAG, "scanAndReport. file: " + file.getName() + ", path: " + file.getAbsolutePath());
+            //}
+
+            String type = "unknown";
+            if (file.getName().endsWith("jni.log")) {
+                type = "jni";
+            } else if (file.getName().endsWith("anr.log")) {
+                type = "anr";
+            } else if (file.getName().endsWith("java.log")) {
+                type = "java";
+            }
+            //else if (file.getName().endsWith("unexp.log")) {
+            //    type = "unexp";
+            //}
+
+            parseTraceFile(context, type, file);
+        }
+    }
+}
