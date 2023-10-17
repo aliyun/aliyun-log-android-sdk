033d508ce00564fcad577280a6f86aeef1f3a5da
gzbleu <gzbleu@gmail.com>
2023-10-17 18:00:50 +0800

    feat: okhttp instrumentation 1.0.1

--- okhttp_instrumentation/src/main/java/com/aliyun/sls/android/okhttp/instrumentation/OkHttpConfiguration.java
+++ okhttp_instrumentation/src/main/java/com/aliyun/sls/android/okhttp/instrumentation/OkHttpConfiguration.java
@@ -1,14 +1,11 @@
 package com.aliyun.sls.android.okhttp.instrumentation;
 
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
@@ -22,14 +19,9 @@ import io.opentelemetry.instrumentation.api.util.VirtualField;
 import io.opentelemetry.instrumentation.okhttp.v3_0.internal.ConnectionErrorSpanInterceptor;
 import io.opentelemetry.instrumentation.okhttp.v3_0.internal.TracingInterceptor;
 import okhttp3.Interceptor;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
-import okio.Buffer;
-import okio.BufferedSource;
 
 /**
  * @author yulong.gyl
@@ -159,93 +151,5 @@ public final class OkHttpConfiguration {
             .addAttributesExtractors(ATTRIBUTES_EXTRACTORS)
             .addOperationMetrics(HttpClientMetrics.get())
             .buildInstrumenter(SpanKindExtractor.alwaysClient());
-    }
-
-    private static class HttpHeadersAttributes implements AttributesExtractor<Request, Response> {
-        private boolean captureRequestHeaders = true;
-        private boolean captureRequestBody = false;
-        private boolean captureResponseHeaders = true;
-        private boolean captureResponseBody = false;
-
-        public void setCaptureRequestHeaders(boolean captureRequestHeaders) {
-            this.captureRequestHeaders = captureRequestHeaders;
-        }
-
-        public void setCaptureRequestBody(boolean captureRequestBody) {
-            this.captureRequestBody = captureRequestBody;
-        }
-
-        public void setCaptureResponseHeaders(boolean captureResponseHeaders) {
-            this.captureResponseHeaders = captureResponseHeaders;
-        }
-
-        public void setCaptureResponseBody(boolean captureResponseBody) {
-            this.captureResponseBody = captureResponseBody;
-        }
-
-        @Override
-        public void onStart(AttributesBuilder attributes, Context parentContext, Request request) {
-            if (captureRequestHeaders && null != request.headers()) {
-                for (String name : request.headers().names()) {
-                    attributes.put("http.request.header." + name, request.header(name));
-                }
-            }
-
-            if (captureRequestBody && null != request.body()) {
-                String body = requestBody2String(request.body());
-                if (null != body) {
-                    attributes.put("http.request.body", body);
-                }
-            }
-        }
-
-        @Override
-        public void onEnd(AttributesBuilder attributes, Context context, Request request, Response response,
-            Throwable error) {
-            if (captureResponseHeaders && null != response.headers()) {
-                for (String name : response.headers().names()) {
-                    attributes.put("http.response.header." + name, response.header(name));
-                }
-            }
-
-            if (captureResponseBody && null != response.body()) {
-                String body = responseBody2String(response.body());
-                if (null != body) {
-                    attributes.put("http.response.body", body);
-                }
-            }
-        }
-
-        private String requestBody2String(RequestBody body) {
-            try {
-                Buffer buffer = new Buffer();
-                body.writeTo(buffer);
-                return buffer.readUtf8();
-            } catch (IOException e) {
-                return null;
-            }
-        }
-
-        private String responseBody2String(ResponseBody responseBody) {
-            try {
-                BufferedSource source = responseBody.source();
-                source.request(Integer.MAX_VALUE);
-                Buffer buffer = source.buffer();
-                //noinspection CharsetObjectCanBeUsed
-                Charset charset = Charset.forName("UTF-8");
-                MediaType contentType = responseBody.contentType();
-                if (null != contentType) {
-                    charset = contentType.charset(charset);
-                }
-                String body = buffer.clone().readString(charset);
-
-                // limit response body to 50 KB
-                final int maxLength = Math.min(50 * 1024, body.length());
-                return body.substring(0, maxLength);
-            } catch (IOException e) {
-                // ignore
-                return null;
-            }
-        }
     }
 }
