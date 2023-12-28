0bac3057c62bb3f6203da32d244a15a05e2c2c6d
gzbleu <gzbleu@gmail.com>
2023-12-28 16:17:25 +0800

    feat: upgrade alinetworkdiagnosis to 0.2.2.0

--- publish-network_diagnosis.sh
+++ publish-network_diagnosis.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-# 定义版本号
-version=2.0.17
-moduleName=sls-android-network-diagnosis
-
-./gradlew :aliyun_sls_android_network_diagnosis:clean -Pversion=${version} -PmoduleName=${moduleName}
-./gradlew :aliyun_sls_android_network_diagnosis:assembleRelease -Pversion=${version} -PmoduleName=${moduleName}
-#./gradlew :aliyun_sls_android_network_diagnosis:publishToSonatype closeAndReleaseStagingRepository -Pversion=${version} -PmoduleName=${moduleName}
-./gradlew :aliyun_sls_android_network_diagnosis:publishToMavenLocal -Pversion=${version} -PmoduleName=${moduleName}
+./gradlew :aliyun_sls_android_network_diagnosis:clean
+./gradlew :aliyun_sls_android_network_diagnosis:assembleRelease
+./gradlew :aliyun_sls_android_network_diagnosis:publishToSonatype closeAndReleaseStagingRepositor
+#./gradlew :aliyun_sls_android_network_diagnosis:publishToMavenLocal
