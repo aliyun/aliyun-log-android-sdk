c5fea878b4de3813821dc31f77c5d83207ffd2f6
gzbleu <gzbleu@gmail.com>
2024-03-07 14:15:30 +0800

    feat: refine gradle scripts

--- publish-webview_instrumentation.sh
+++ publish-webview_instrumentation.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# 定义版本号
-version=1.2.0-dev.1
-moduleName=sls-android-webview-instrumentation
 
 ./gradlew :web_instrumentation:clean -Pversion=${version} -PmoduleName=${moduleName}
 ./gradlew :web_instrumentation:assembleRelease -Pversion=${version} -PmoduleName=${moduleName}
-#./gradlew :web_instrumentation:publishToSonatype closeAndReleaseStagingRepository -Pversion=${version} -PmoduleName=${moduleName}
-./gradlew :web_instrumentation:publishToMavenLocal -Pversion=${version} -PmoduleName=${moduleName}
+./gradlew :web_instrumentation:publishToSonatype closeAndReleaseStagingRepository -Pversion=${version} -PmoduleName=${moduleName}
+#./gradlew :web_instrumentation:publishToMavenLocal -Pversion=${version} -PmoduleName=${moduleName}
